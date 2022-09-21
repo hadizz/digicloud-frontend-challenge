035ca8e40b19277b139434ed83575116256cfb5e
Hadi Zare <hadizareoriginal@gmail.com>
2022-09-21 21:04:21 +0430

    fix(build): remove service and log

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-import React, { useEffect } from 'react';
-import randomApiServices from './Services/RandomApiSerivce';
+import React from 'react';
 
 function App() {
-  useEffect(() => {
-    randomApiServices.getUsersList({ results: 100 }).then((res) => {
-      console.log(res.data.results);
-    });
-  }, []);
   return <div>test http service</div>;
 }
 
